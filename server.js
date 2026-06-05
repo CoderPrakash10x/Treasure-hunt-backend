aabedafb569d1be42fd69f019e86584c60ccfc62
CoderPrakash10x <mr.prakash10x@gmail.com>
2026-06-05 22:00:52 +0530

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 // ── Security ──────────────────────────────────────────────────────────────
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin:'https://treasure-hunt-frontend-coral.vercel.app/' || "",
   credentials: true,
 }));
 
